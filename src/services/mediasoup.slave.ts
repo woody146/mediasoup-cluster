c9e8590e8122bab1269688d2b1a7fd6154430dcd
woody <woodpecker146@gmail.com>
2023-06-07 19:53:15 +0700

    feat: add MediasoupSlave entity

---
+++ src/services/mediasoup.slave.ts
@@ -0,0 +1,20 @@
+import { constants } from '../constants';
+import { MediasoupSlave } from '../entities';
+import { BaseService } from './base';
+
+export class MediasoupSlaveService extends BaseService {
+  add(data: {
+    internalHost: string;
+    externalHost: string;
+    for: string; // consumer | producer
+    apiPort: number;
+    maxPeer?: number;
+  }) {
+    const mediasoupSlave = new MediasoupSlave();
+    if (![constants.CONSUMER, constants.PRODUCER].includes(data.for)) {
+      throw new Error('Invalid for param');
+    }
+    Object.assign(mediasoupSlave, data);
+    this.dataSource.getRepository(MediasoupSlave).save(mediasoupSlave);
+  }
+}
