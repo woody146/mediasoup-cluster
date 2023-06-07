68ad8ed7cf28bc9f65e8115f618704be9bb26827
woody <woodpecker146@gmail.com>
2023-06-07 16:45:28 +0700

    feat: add run_slave

---
+++ src/utils/bootstrap.ts
@@ -0,0 +1,17 @@
+import Fastify from 'fastify';
+import dotenv from 'dotenv';
+import { getDataSource } from './datasource.js';
+
+export async function startServer() {
+  const server = Fastify({});
+  dotenv.config();
+
+  try {
+    const port = process.env.PORT;
+    await getDataSource().initialize();
+    await server.listen({ port: port ? parseInt(port) : 3000 });
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
