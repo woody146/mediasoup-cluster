a81257213661da4f82c724a5136e0bef27114098
woody <woodpecker146@gmail.com>
2023-06-07 15:42:25 +0700

    feat: add start master script

---
+++ src/datasource.ts
@@ -0,0 +1,19 @@
+import { DataSource } from 'typeorm';
+import * as entitiesMap from './entities/index.js';
+
+export const entities = Object.values(entitiesMap);
+
+let _dataSource: DataSource;
+
+export function getDataSource() {
+  if (!_dataSource) {
+    _dataSource = new DataSource({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      synchronize: !!process.env.DATABASE_SYNC,
+      logging: !!process.env.DATABASE_LOGGING,
+      entities,
+    });
+  }
+  return _dataSource;
+}
