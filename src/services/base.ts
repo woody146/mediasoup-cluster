c6647fb0d65c0ddf9a1c468ee32750da9dadaaaf
woody <woodpecker146@gmail.com>
2023-06-08 11:13:11 +0700

    feat: add MediasoupRouterService

--- src/services/base.ts
+++ src/services/base.ts
@@ -2,4 +2,10 @@ import { type DataSource } from 'typeorm';
 
 export class BaseService {
   constructor(protected dataSource: DataSource) {}
+
+  createService<T extends BaseService>(
+    serviceClass: new (dataSource: DataSource) => T
+  ) {
+    return new serviceClass(this.dataSource);
+  }
 }
