441d7d3d32b353d60466017504412173a21baed1
woody <woodpecker146@gmail.com>
2023-06-08 15:45:31 +0700

    chore: remove old rooms after restart

--- src/run_slave.ts
+++ src/run_slave.ts
@@ -2,6 +2,7 @@ import 'node-fetch';
 
 import apiRouters from './apis/slave/index.js';
 import {
+  MediasoupRoomService,
   MediasoupSlaveService,
   mediasoupWorkerManager,
 } from './services/index.js';
@@ -10,6 +11,8 @@ import { startServer, getDataSource } from './utils/index.js';
 async function bootstrap() {
   await startServer(apiRouters);
   try {
+    // remove old room of dead slave
+    await new MediasoupRoomService(getDataSource()).removeAll();
     await mediasoupWorkerManager.init();
     await new MediasoupSlaveService(getDataSource()).addFromEnv();
   } catch (e) {}
