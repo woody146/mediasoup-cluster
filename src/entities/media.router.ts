484bbbdd0f576861338a7a24a77d0e9d194638ca
woody <woodpecker146@gmail.com>
2023-06-13 15:55:57 +0700

    feat: add MediaRouter model

---
+++ src/entities/media.router.ts
@@ -0,0 +1,39 @@
+import {
+  Entity,
+  BaseEntity,
+  CreateDateColumn,
+  Column,
+  ManyToOne,
+  PrimaryColumn,
+} from 'typeorm';
+import { MediaSlave } from './media.slave.js';
+import { MediaRoom } from './media.room.js';
+
+@Entity()
+export class MediaRouter extends BaseEntity {
+  @PrimaryColumn('uuid')
+  id!: string;
+
+  @Column('integer')
+  slaveId!: number;
+
+  @ManyToOne(() => MediaSlave, (slave) => slave.routers, {
+    onDelete: 'CASCADE',
+  })
+  slave!: MediaSlave;
+
+  @Column('uuid')
+  roomId!: string;
+
+  @ManyToOne(() => MediaRoom, (room) => room.routers, { onDelete: 'CASCADE' })
+  room!: MediaRoom;
+
+  @Column('text')
+  sourceRouterId!: string;
+
+  @Column({ type: 'jsonb', nullable: true })
+  metadata?: any;
+
+  @CreateDateColumn()
+  createDate!: Date;
+}
