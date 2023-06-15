286bdc2302558dbabb78179addc7a7483451e9bb
woody <woodpecker146@gmail.com>
2023-06-15 09:01:46 +0700

    feat: add RouterService.checkToPipe

--- src/services/room.ts
+++ src/services/room.ts
@@ -23,28 +23,28 @@ export class RoomService extends BaseService {
       await this.dataSource.getRepository(MediaRoom).save(mediaRoom);
       return { ...result, id: mediaRoom.id };
     }
-    throw new ServiceError(404, 'Slave not found');
   }
 
   async get(data: { roomId: string }) {
-    return this.dataSource.getRepository(MediaRoom).findOne({
+    const room = await this.dataSource.getRepository(MediaRoom).findOne({
       relations: { slave: true },
       where: { id: data.roomId },
     });
+    if (room) {
+      return room;
+    }
+    throw new ServiceError(404, 'Room not found');
   }
 
   async getCapabilities(data: { roomId: string }) {
     const room = await this.get(data);
-    if (room) {
-      const result = await fetchApi({
-        host: room.slave.internalHost,
-        port: room.slave.apiPort,
-        path: '/routers/:routerId',
-        method: 'GET',
-        data: { routerId: room.routerId },
-      });
-      return { ...result, id: data.roomId };
-    }
-    throw new ServiceError(404, 'Room not found');
+    const result = await fetchApi({
+      host: room.slave.internalHost,
+      port: room.slave.apiPort,
+      path: '/routers/:routerId',
+      method: 'GET',
+      data: { routerId: room.routerId },
+    });
+    return { ...result, id: data.roomId };
   }
 }
