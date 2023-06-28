41ab0d5ff053500d4d235647c6e769ba8c1ae2c8
woody <woodpecker146@gmail.com>
2023-06-28 21:14:34 +0700

    chore: remove options staticFolder

--- src/run_slave.ts
+++ src/run_slave.ts
@@ -1,23 +1,11 @@
 import 'node-fetch';
-import { dirname, join } from 'path';
-import { fileURLToPath } from 'url';
 
 import apiRouters from './apis/slave.js';
 import { WorkerService, mediasoupWorkerManager } from './services/index.js';
 import { startServer, getDataSource } from './utils/index.js';
-import { constants } from './constants.js';
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 async function bootstrap() {
-  await startServer(
-    apiRouters,
-    process.env.SLAVE_FOR === constants.PRODUCER
-      ? {
-          staticFolder: join(__dirname, '../static/slave_producer'),
-        }
-      : {}
-  );
+  await startServer(apiRouters);
 
   await mediasoupWorkerManager.init();
 
